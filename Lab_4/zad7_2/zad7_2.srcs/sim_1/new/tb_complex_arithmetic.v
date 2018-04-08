9fca6e3a6e418fc8fd12b0435d603c354d055549
Bartłomiej Styczeń <daltar96@gmail.com>
2018-04-08 10:29:15 +0200

    Finished MATLAB model, for testing

--- Lab_4/zad7_2/zad7_2.srcs/sim_1/new/tb_complex_arithmetic.v
+++ Lab_4/zad7_2/zad7_2.srcs/sim_1/new/tb_complex_arithmetic.v
@@ -3,12 +3,16 @@
 module tb_complex_arithmetic
     (
     );
+    // inputs
     wire signed [17:0] A = 18'h39ba9;
     wire signed [7:0] B = 8'h3b;
     wire signed [11:0] C = 12'hd8a;
-
+    wire signed [7:0] D;
+    wire signed [13:0] E;
+    wire signed [18:0] F;
+    // outputs
     wire signed [30:0] Y;
-    
+    // signals
     wire clk;
     wire ce = 1'b1;
     
@@ -24,6 +28,9 @@ module tb_complex_arithmetic
         .A(A),
         .B(B),
         .C(C),
+        .D(D),
+        .E(E),
+        .F(F),
         .Y(Y)
     );
 endmodule
