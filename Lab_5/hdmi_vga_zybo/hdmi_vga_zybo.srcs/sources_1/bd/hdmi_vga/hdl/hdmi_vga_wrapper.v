68ddbf913b7e5f4e39808fcceb03edc97dc99080
lsriw <lsriw@debian>
2018-04-11 09:29:06 +0200

    Files from laboratory

---
+++ Lab_5/hdmi_vga_zybo/hdmi_vga_zybo.srcs/sources_1/bd/hdmi_vga/hdl/hdmi_vga_wrapper.v
@@ -0,0 +1,92 @@
+//Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2017.4 (lin64) Build 2086221 Fri Dec 15 20:54:30 MST 2017
+//Date        : Wed Apr 11 09:26:12 2018
+//Host        : debian running 64-bit Debian GNU/Linux 9.3 (stretch)
+//Command     : generate_target hdmi_vga_wrapper.bd
+//Design      : hdmi_vga_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module hdmi_vga_wrapper
+   (hdmi_hdp,
+    hdmi_in_clk_n,
+    hdmi_in_clk_p,
+    hdmi_in_data_n,
+    hdmi_in_data_p,
+    hdmi_in_ddc_scl_io,
+    hdmi_in_ddc_sda_io,
+    hdmi_out_en,
+    sys_clock,
+    vga_pBlue,
+    vga_pGreen,
+    vga_pHSync,
+    vga_pRed,
+    vga_pVSync);
+  output [0:0]hdmi_hdp;
+  input hdmi_in_clk_n;
+  input hdmi_in_clk_p;
+  input [2:0]hdmi_in_data_n;
+  input [2:0]hdmi_in_data_p;
+  inout hdmi_in_ddc_scl_io;
+  inout hdmi_in_ddc_sda_io;
+  output [0:0]hdmi_out_en;
+  input sys_clock;
+  output [4:0]vga_pBlue;
+  output [5:0]vga_pGreen;
+  output vga_pHSync;
+  output [4:0]vga_pRed;
+  output vga_pVSync;
+
+  wire [0:0]hdmi_hdp;
+  wire hdmi_in_clk_n;
+  wire hdmi_in_clk_p;
+  wire [2:0]hdmi_in_data_n;
+  wire [2:0]hdmi_in_data_p;
+  wire hdmi_in_ddc_scl_i;
+  wire hdmi_in_ddc_scl_io;
+  wire hdmi_in_ddc_scl_o;
+  wire hdmi_in_ddc_scl_t;
+  wire hdmi_in_ddc_sda_i;
+  wire hdmi_in_ddc_sda_io;
+  wire hdmi_in_ddc_sda_o;
+  wire hdmi_in_ddc_sda_t;
+  wire [0:0]hdmi_out_en;
+  wire sys_clock;
+  wire [4:0]vga_pBlue;
+  wire [5:0]vga_pGreen;
+  wire vga_pHSync;
+  wire [4:0]vga_pRed;
+  wire vga_pVSync;
+
+  IOBUF hdmi_in_ddc_scl_iobuf
+       (.I(hdmi_in_ddc_scl_o),
+        .IO(hdmi_in_ddc_scl_io),
+        .O(hdmi_in_ddc_scl_i),
+        .T(hdmi_in_ddc_scl_t));
+  IOBUF hdmi_in_ddc_sda_iobuf
+       (.I(hdmi_in_ddc_sda_o),
+        .IO(hdmi_in_ddc_sda_io),
+        .O(hdmi_in_ddc_sda_i),
+        .T(hdmi_in_ddc_sda_t));
+  hdmi_vga hdmi_vga_i
+       (.hdmi_hdp(hdmi_hdp),
+        .hdmi_in_clk_n(hdmi_in_clk_n),
+        .hdmi_in_clk_p(hdmi_in_clk_p),
+        .hdmi_in_data_n(hdmi_in_data_n),
+        .hdmi_in_data_p(hdmi_in_data_p),
+        .hdmi_in_ddc_scl_i(hdmi_in_ddc_scl_i),
+        .hdmi_in_ddc_scl_o(hdmi_in_ddc_scl_o),
+        .hdmi_in_ddc_scl_t(hdmi_in_ddc_scl_t),
+        .hdmi_in_ddc_sda_i(hdmi_in_ddc_sda_i),
+        .hdmi_in_ddc_sda_o(hdmi_in_ddc_sda_o),
+        .hdmi_in_ddc_sda_t(hdmi_in_ddc_sda_t),
+        .hdmi_out_en(hdmi_out_en),
+        .sys_clock(sys_clock),
+        .vga_pBlue(vga_pBlue),
+        .vga_pGreen(vga_pGreen),
+        .vga_pHSync(vga_pHSync),
+        .vga_pRed(vga_pRed),
+        .vga_pVSync(vga_pVSync));
+endmodule
